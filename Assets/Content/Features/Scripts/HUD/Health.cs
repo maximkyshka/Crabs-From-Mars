d04f0db9e50ec390c6fd76221f218815d920e120
Bobick2023 <140235035+Bobick2023@users.noreply.github.com>
2025-09-19 18:40:02 +0300

    Здоров'я зробив
    лишилось голод

--- Assets/Content/Features/Scripts/HUD/Health.cs
+++ Assets/Content/Features/Scripts/HUD/Health.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class Health : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 100;
     [SerializeField] private int health;
     [SerializeField] private Slider HealthSlider;
+    [SerializeField] private TMP_Text healthText;
     [SerializeField] private GameObject DeathScreen;
 
     void Start()
@@ -13,13 +14,14 @@ public class Health : MonoBehaviour
         health = maxHealth;
         HealthSlider.maxValue = maxHealth;
         HealthSlider.value = health;
+        healthText.text = health.ToString();
     }
 
     public void TakeDamage(int damage)
     {
         health -= damage;
         HealthSlider.value = health;
-
+        healthText.text = health.ToString();
         if (health <= 0)
         {
             Destroy(gameObject);
